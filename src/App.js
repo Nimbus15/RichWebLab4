5c01b0e2eb0b3cfc29c106d03feeb70b5cdd3357
Nimbus15 <Alex.ZBP2456@GMAIL.COM>
2022-12-28 19:35:20 +0000

    allow creating more notes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 import Note from "./components/Note";
-class App extends Component {
-  
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      notes: ["12345678", "546328"],
+    };
 
-  render() {
+    this.addNote = this.addNote.bind(this);
+    this.renderNodeElement = this.renderNodeElement.bind(this);
+   }
 
-    return (
-      <div>
-        <h1>Hello World</h1>
+    addNote() {
+      this.setState({
+        notes: this.state.notes.concat(new Date().getTime()),
+      });
+    }
+
+
+    renderNodeElement(id) {
+      return (
         <div>
-          <Note/>
+          <Note
+            id={id}
+          />
+        </div>
+      );
+    }
+
+    render() {
+      return (
+        <div>
+          <h1>Notes App</h1>
+          <div>
+            <button onClick={this.addNote}>Add Note</button>
+          </div>
+          <div>
+            {this.state.notes.map((id) => (
+              <Note
+                id={id}
+              />
+            ))}
         </div>
       </div>
     );
   }
 }
-
-export default App;
